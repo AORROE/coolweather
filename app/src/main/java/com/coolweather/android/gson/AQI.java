6f99bdd49cf754b5f442147a77e2dfbe4f50b1bd
arrow <784191744@qq.com>
2018-02-23 21:05:34 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by arrow on 2018/2/23.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
